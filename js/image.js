9d4783a23ec83f2bd6d59b5cbdcafa826b2fbf94
Luna <20017802+boppinluna@users.noreply.github.com>
2024-07-24 22:00:19 +0100

    Update image.js

--- js/image.js
+++ js/image.js
@@ -1,6 +1,6 @@
 var images = [
     'LHlnm1R.png',
-    'rwRzJ14.jpg', 'DDLuMXM.png', 'cq3IynI.jpg', 'HzieDba.jpg', 'pYA8fRa.jpg',
+    'rwRzJ14.jpg', 'DDLuMXM.png', 'cq3IynI.jpg', 'HzieDba.jpg', 'xv6jHZK.png',
     'm7VEGQ2.jpg', 'hoGtxLr.jpg', 'Hd6mDRV.jpg', '4Vf90NL.jpg', 'YyFbQLh.jpg',
     'XjNNedB.jpg', 'DvkXAlO.jpg', 'WAo642t.jpg', 'vzsX9MK.jpg', 'w90x5s3.jpg',
     'FCy2SNR.jpg', '6cIx6jN.jpg', 'fdBM2ZD.jpg', 'OzvySyW.jpg', 'yBmjLlZ.jpg',
@@ -18,7 +18,15 @@ var images = [
     'nhOEycJ.png', 'fhMkOgR.png', '3rOXhRz.png', '9FHaigm.png', 'xR9fwLr.png',
     'LbojF9u.png', 'ikLFiVh.png', 'xaIZfkM.png', 'Yqi1oqb.png', 'PpfJ9ec.png',
     'KDGEKm5.png', 'xazFGlN.png', 'VKpv8Bu.png', 'T8DX2bx.png', 'wOuMTPk.png',
-    'tUaw9qo.png', 'dZ0GrID.png', 'pdRk5iV.png', 'FJ9q6D3.png', 'HfY4wri.png'
+    'tUaw9qo.png', 'dZ0GrID.png', 'pdRk5iV.png', 'FJ9q6D3.png', 'HfY4wri.png',
+    'kh5ovUP.png', 'w0NPqET.png', 'p1yPP8p.jpeg', 'kzJhwjO.jpeg', '540At7x.png',
+    'st5NAUn.png', 'U9TYES9.png', '4lj961h.png', 'e9xxtLp.png', 'wNWx8CQ.png', '1OPT38V.png',
+    'MMGGzUr.png', 'dPYxscb.png', 'ImrGcrW.png', 'hQHetZ9.png', 'wtUGGGG.png', '63gkef3.png',
+    'ePwbHWj.png', 'nAeQ8yp.png', 'oiJVOLd.png', 'jru9dXO.png', 'jyGV0uo.png', 'nAsyacG.png',
+    'XyVGfPC.png', 'UW7yxyS.jpeg', 'LdWyqLe.jpeg', 'R27hkz4.jpeg', '3Y5ghiA.jpeg', '4OxodqJ.png',
+    'qSTefQN.jpeg',  'hLZTEtq.jpeg',  'dbhDsPo.jpeg',  'Tizu7NH.jpeg',  'm1izdfd.jpeg',
+    'EuaEnkV.jpeg',  'C1Q48SL.jpeg',  'Z8mcg5j.jpeg',  '7yIhj0k.jpeg',  'IgyYoyI.jpeg', 'iCHHyVb.jpeg',
+    '1fdIFvI.jpeg', 'BtLEDm3.jpeg', 'RTVnxbr.jpeg'
 ];
 var image = images[Math.floor(Math.random() * images.length)];
 $('body').css({
